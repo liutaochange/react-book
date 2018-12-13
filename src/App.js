4211cee7874a8498eddb6edc79d4266c291672ad
liutaochange <liutaochange@163.com>
2018-12-13 16:33:49 +0800

    header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { GlobalStyle } from './style.js'
+import Header from './components/header/index.js'
 
 class App extends Component {
   render() {
     return (
-      <p>
-        hello react
-      </p>
+      <Fragment>
+        <GlobalStyle />
+        <Header />
+      </Fragment>
     );
   }
 }

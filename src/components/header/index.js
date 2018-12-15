4cf3f842aa44a96bc8ac108263b159a845baf8a1
liutaochange <liutaochange@163.com>
2018-12-15 17:24:51 +0800

    change component way

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HeaderWamp, Logo, Nav, NavItem, SearchWamp, Navsearch, Addition, Button } from './style.js';
 import { getInputFocusAction, getInputBlurAction } from '../../store/actionCreate.js';
 import { CSSTransition } from 'react-transition-group';
 import { connect } from 'react-redux';
-class Header extends Component {
-  render() {
-    const {focused, handleFocus, handleBlur} = this.props
-    return (
-      <HeaderWamp>
-        <Logo href='/' />
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载App</NavItem>
-          <SearchWamp>
-            <CSSTransition
-              in={focused}
-              timeout={200}
-              classNames="slider"
-              >
-              <Navsearch className={focused ? 'focused' : ''} onFocus={handleFocus} onBlur={handleBlur}/>
-            </CSSTransition>
-            <i className={focused ? 'iconfont focused' : 'iconfont'}>&#xe60a;</i>
-          </SearchWamp>
-          <NavItem className="right">登录</NavItem>
-          <NavItem className="right">
-            <i className="iconfont">&#xe636;</i>
-          </NavItem>
-        </Nav>
-        <Addition>
-          <Button className="write">
-            <i className="iconfont">&#xe603;</i>
-            写文章
-          </Button>
-          <Button className="signUp">注册</Button>
-        </Addition>
-      </HeaderWamp>
-    )
-  }
+const Header = (props) => {
+  const {focused, handleFocus, handleBlur} = props
+  return (
+    <HeaderWamp>
+      <Logo href='/' />
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载App</NavItem>
+        <SearchWamp>
+          <CSSTransition
+            in={focused}
+            timeout={200}
+            classNames="slider"
+            >
+            <Navsearch className={focused ? 'focused' : ''} onFocus={handleFocus} onBlur={handleBlur}/>
+          </CSSTransition>
+          <i className={focused ? 'iconfont focused' : 'iconfont'}>&#xe60a;</i>
+        </SearchWamp>
+        <NavItem className="right">登录</NavItem>
+        <NavItem className="right">
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+      </Nav>
+      <Addition>
+        <Button className="write">
+          <i className="iconfont">&#xe603;</i>
+          写文章
+        </Button>
+        <Button className="signUp">注册</Button>
+      </Addition>
+    </HeaderWamp>
+  )
 }
 const mapStateToProps = (state) =>  {
   return { 

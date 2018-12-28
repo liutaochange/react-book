0652c10394337cfb8394ff7f29674c44575fb563
liutaochange <liutaochange@163.com>
2018-12-28 10:29:47 +0800

    delete video

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,11 +5,10 @@ import { actionCreate } from './store/index.js';
 import { Redirect } from "react-router-dom";
 class Detail extends PureComponent {
   render() {
-    const { login, submitLoginEvent, play } = this.props
+    const { login, submitLoginEvent } = this.props
     if (!login) {
       return (
         <LoginWrapper>
-          <video controls ref={(video) => { this.audioValue = video; }}  loop  autoPlay="autoplay" name="media" src="https://oss.guzheng.cn/mp3/gz111212/mp3/hu10112/chenleishi-13liushui.mp3" onLoad ={() => play(this.audioValue)}></video>
           <LoginWamp>
             <LoginItem>
               <input placeholder="手机号或邮箱" type="text" name="" ref={name => { this.useName = name }}/>
@@ -35,9 +34,6 @@ const mapDispatchToProps = (dispatch) => {
   return {
     submitLoginEvent(userName, passWord) {
       dispatch(actionCreate.submitLogin(userName, passWord))
-    },
-    paly(audioValue) {
-      audioValue.play()
     }
   }
 }

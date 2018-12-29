aba2aac05ba9bcba313a341d7995a5ba80cc455e
liutaochange <liutaochange@163.com>
2018-12-29 11:12:43 +0800

    import koa-router

--- service/index.js
+++ service/index.js
@@ -1,17 +1,28 @@
 const Koa = require('koa');
 const app = new Koa();
+const Router = require('koa-router');
 const {
   connect,
   initSchemas
 } = require('./mongoose/index.js');
 
-;(async () => {
+// 初始化路由
+let router = new Router();
+// 装载所有子路由
+const list = require('./app/list.js');
+router.use('/list', list.routes(), list.allowedMethods());
+router.use('/page', page.routes(), page.allowedMethods());
+
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods());
+
+(async () => {
   await connect()
   initSchemas()
 })()
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
-});
+})
 
-app.listen(6000);
+app.listen(6001);
